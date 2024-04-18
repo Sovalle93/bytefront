2b64825b56d0211abb68aaa15a9490e7b1329cba
Santiago Ovalle <133941360+Sovalle93@users.noreply.github.com>
2024-04-18 18:19:39 -0400

    Update constans.js

--- front/src/config/constans.js
+++ front/src/config/constans.js
@@ -8,6 +8,7 @@ export const ENDPOINT = {
   enterprise: `${URLBASE}/enterprise`,
   searchapp: `${URLBASE}/searchapp`,
   createjobs:`${URLBASE}/createjobs`,
+  businesses:`${URLBASE}/businesses`,
   profile:`${URLBASE}/profile`,
 }
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gi;
